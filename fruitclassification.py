8979d46a91a10b6474de2ac2396c0ca5906c7a49
Jimmy Connelly <j.connelly2503@gmail.com>
2023-03-06 12:21:27 -0500

    First graphing functionality

--- fruitclassification.py
+++ fruitclassification.py
@@ -1,18 +1,21 @@
-#This script is inspired by Sebastian Lague's Youtube Nerual Network Tutorial
+import ipywidgets
+import numpy as np
+import matplotlib.pyplot as plt
 
+#This script is inspired by Sebastian Lague's Youtube Neural Network Tutorial
 
 #Weights of neural network for first output
-weight1_1, weight2_1 = 0.0, 0.0
+w1_1, w2_1 = 0.0, 0.0
 
 #Weights of neural network for second output
-weight1_2, weight2_2 = 0.0, 0.0
+w1_2, w2_2 = 0.0, 0.0
 
 #Bias
-bias1, bias2 = 0.0, 0.0
+b1, b2 = 0.0, 0.0
 
 def classify(input1, input2):
-    output1 = weight1_1 * input1 + weight2_1 * input2 + bias1
-    output2 = weight1_2 * input1 + weight2_2 * input2 + bias2
+    output1 = w1_1 * input1 + w2_1 * input2 + b1
+    output2 = w1_2 * input1 + w2_2 * input2 + b2
 
     return output1 > output2
 
@@ -25,6 +28,23 @@ def visualize(graphX, graphY):
         pass
     else:
         pass
+
+def plot_fct(w=1):
+    x = np.random.uniform(0, 5, size=100)
+    y = w * np.random.normal(size=100)
+
+    for i in range(x.size):
+        if y[i] > 0:
+            plt.scatter(x[i], y[i], color= 'red')
+        else:
+            plt.scatter(x[i], y[i], color ='blue')
+    
+
+
+#add slider functionality
+plt.show()
+    
+
 
 
 
